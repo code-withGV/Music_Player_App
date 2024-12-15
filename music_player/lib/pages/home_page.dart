01aa0cde25c65ea2656a821f188d2950407b4ee6
code-withGV <gauravvashistha452@gmail.com>
2024-12-15 21:46:19 +0530

    commit1

---
+++ music_player/lib/pages/home_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/components/my_drawer.dart';
+import 'package:music_player/models/playlist_provider.dart';
+import 'package:music_player/models/song.dart';
+import 'package:music_player/pages/song_page.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final dynamic playlistProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    playlistProvider = Provider.of<PlaylistProvider>(context, listen: false);
+    //playlistProvider.loadPlaylist();
+  }
+
+  void goToSong(int index) {
+    playlistProvider.setCurrentSongIndex(index);
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const SongPage()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        title: Text(
+          " P L A Y L I S T",
+          style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
+        ),
+        elevation: 0,
+      ),
+      drawer: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        child: const MyDrawer(),
+      ),
+      body: Consumer<PlaylistProvider>(
+        builder: (context, value, child) {
+          final List<Song> playlist = value.playlist;
+          return ListView.builder(
+            itemCount: playlist.length,
+            itemBuilder: (context, index) {
+              final Song song = playlist[index];
+
+              return ListTile(
+                title: Text(song.songName,
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.inversePrimary)),
+                subtitle: Text(
+                  song.artistName,
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary),
+                ),
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(
+                      20.0), // Set the rounded corner radius here
+                  child: Image.asset(
+                    song.albumArtImagePath, // Replace with your image url
+                    width: 100, // Set your width
+                    height: 200, // Set your height
+                    fit: BoxFit
+                        .cover, // Use BoxFit.cover to keep the image aspect ratio
+                  ),
+                ),
+                onTap: (() => goToSong(index)),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ music_player/lib/pages/song_page.dart
@@ -0,0 +1,230 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/components/neu_box.dart';
+import 'package:music_player/models/playlist_provider.dart';
+import 'package:provider/provider.dart';
+
+class SongPage extends StatelessWidget {
+  const SongPage({super.key});
+
+  String formatTime(Duration duration) {
+    String twoDigitSeconds =
+        duration.inSeconds.remainder(60).toString().padLeft(2, '0');
+    String formattedTime = "${duration.inMinutes}:$twoDigitSeconds";
+
+    return formattedTime;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PlaylistProvider>(
+        builder: (context, playlistProvider, child) {
+      final playlist = playlistProvider.playlist;
+
+      final currentSong = playlist[playlistProvider.currentSongIndex ?? 0];
+
+      return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: SafeArea(
+          child: Padding(
+            padding:
+                const EdgeInsets.only(left: 25.0, right: 25.0, bottom: 25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // App bar
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Back button
+                    IconButton(
+                      onPressed: () => Navigator.pop(context),
+                      icon: Icon(Icons.arrow_back,
+                          color: Theme.of(context).colorScheme.inversePrimary),
+                    ),
+
+                    // Title
+                    Text(" P L A Y L I S T ",
+                        style: TextStyle(
+                            color:
+                                Theme.of(context).colorScheme.inversePrimary)),
+
+                    // Menu button
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.menu,
+                          color: Theme.of(context).colorScheme.inversePrimary),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 25),
+
+                // Album art work
+                NeuBox(
+                  child: Column(
+                    children: [
+                      // Image
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Image.asset(currentSong.albumArtImagePath),
+                      ),
+
+                      // Song and artist name
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  currentSong.songName,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20.0,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .inversePrimary),
+                                ),
+                                Text(currentSong.artistName,
+                                    style: TextStyle(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .inversePrimary)),
+                              ],
+                            ),
+
+                            // Heart icon
+                            const Icon(
+                              Icons.favorite,
+                              color: Colors.red,
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 25.0,
+                ),
+
+                // Song duration progress
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // Start time
+                          Text(formatTime(playlistProvider.currentDuration),
+                              style: TextStyle(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .inversePrimary)),
+
+                          // Shuffle icon
+                          Icon(Icons.shuffle,
+                              color:
+                                  Theme.of(context).colorScheme.inversePrimary),
+
+                          // Repeat icon
+                          Icon(Icons.repeat,
+                              color:
+                                  Theme.of(context).colorScheme.inversePrimary),
+
+                          // End time
+                          Text(formatTime(playlistProvider.totalDuration),
+                              style: TextStyle(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .inversePrimary)),
+                        ],
+                      ),
+                    ),
+                    SliderTheme(
+                      data: SliderTheme.of(context).copyWith(
+                        thumbShape:
+                            const RoundSliderThumbShape(enabledThumbRadius: 0),
+                      ),
+                      child: Slider(
+                        min: 0,
+                        max:
+                            playlistProvider.totalDuration.inSeconds.toDouble(),
+                        value: playlistProvider.currentDuration.inSeconds
+                            .toDouble(),
+                        activeColor: Colors.green,
+                        onChanged: (value) {
+                          // During when the user is sliding around
+                        },
+                        onChangeEnd: (value) {
+                          // Sliding has finished, go to that position in song duration
+                          playlistProvider
+                              .seek(Duration(seconds: value.toInt()));
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 25.0),
+                    Row(
+                      children: [
+                        // Skip previous
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: playlistProvider.playPreviousSong,
+                            child: NeuBox(
+                              child: Icon(Icons.skip_previous,
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .inversePrimary),
+                            ),
+                          ),
+                        ),
+
+                        const SizedBox(width: 20.0),
+
+                        // Play pause
+                        Expanded(
+                          flex: 2,
+                          child: GestureDetector(
+                            onTap: playlistProvider.pauseOrResume,
+                            child: NeuBox(
+                              child: Icon(
+                                  playlistProvider.isPlaying
+                                      ? Icons.pause
+                                      : Icons.play_arrow,
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .inversePrimary),
+                            ),
+                          ),
+                        ),
+
+                        const SizedBox(width: 20.0),
+
+                        // Skip forward
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: playlistProvider.playNextSong,
+                            child: NeuBox(
+                              child: Icon(Icons.skip_next,
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .inversePrimary),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    });
+  }
+}
